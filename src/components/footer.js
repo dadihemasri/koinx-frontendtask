ea9e4656cfdcca19a51fb5e6b28d482856f81dc3
dadihemasri <dadihemasri1202@gmail.com>
2024-02-28 18:38:39 +0530

    initial Commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,208 +1,85 @@
-import React, { useEffect } from 'react';
-import './footer.css'; 
+import React, { useState, useEffect } from 'react';
+import './footer.css'; // Import the CSS file for styling
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+const FooterCarousel = () => {
+  const [trendingCoins, setTrendingCoins] = useState([]);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const scrollAmount = 300; // Adjust this to the width of a single carousel item
 
+  useEffect(() => {
+    const fetchTrendingCoins = async () => {
+      try {
+        const response = await fetch('https://api.coingecko.com/api/v3/search/trending');
+        const data = await response.json();
+        setTrendingCoins(data.coins);
+      } catch (error) {
+        console.error('Error fetching trending coins:', error);
+      }
+    };
 
-function Footer() {
-    useEffect(() => {
-        const carousel = document.querySelector('.carousel');
-        const prevButton = document.querySelector('.prev-button');
-        const nextButton = document.querySelector('.next-button');
-        const scrollAmount = 300; 
+    fetchTrendingCoins();
+  }, []); // Runs once on component mount
 
-        const scrollCarousel = (scrollOffset) => {
-            carousel.scrollBy({
-                left: scrollOffset,
-                behavior: 'smooth'
-            });
-        };
+  const handlePrevButtonClick = () => {
+    setScrollPosition((prev) => Math.max(0, prev - scrollAmount));
+  };
 
-        const handlePrevButtonClick = () => {
-            scrollCarousel(-scrollAmount);
-        };
+  const handleNextButtonClick = () => {
+    setScrollPosition((prev) => prev + scrollAmount);
+  };
 
-        const handleNextButtonClick = () => {
-            scrollCarousel(scrollAmount);
-        };
-
-        prevButton.addEventListener('click', handlePrevButtonClick);
-        nextButton.addEventListener('click', handleNextButtonClick);
-
-        
-        carousel.scrollLeft = scrollAmount * 2;
-
-        return () => {
-       
-            prevButton.removeEventListener('click', handlePrevButtonClick);
-            nextButton.removeEventListener('click', handleNextButtonClick);
-        };
-    }, []);
-
-
-
-    return (
-        <div className="footer">
-            <h3 className="you-may-like-heading">You may like this</h3>
-            <div className="carousel-container">
-                <button className="nav-button prev-button">&#10094;</button>
-                <div className="carousel">
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                           
-                            <span>Ethereum</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$200</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                        
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                </div>
-                <button className="nav-button next-button">&#10095;</button>
+  return (
+    <div>
+      <div className="footer-carousel">
+        <h3 className="you-may-like-heading">You may like this</h3>
+        <button className="nav-button prev-button" onClick={handlePrevButtonClick}>
+          &#10094;
+        </button>
+        <div className="carousel-container" style={{ transform: `translateX(-${scrollPosition}px)` }}>
+          {trendingCoins.map((coin) => (
+            <div className="carousel-item" key={coin.item.id}>
+              <div className="crypto-item">
+                <img src={coin.item.thumb} alt={`${coin.item.name} Logo`} />
+                <span>{coin.item.symbol}</span>
+              </div>
+              <div className="price-info">
+                <span>{coin.item.data.price}</span>
+                <img src={coin.item.sparkline} alt={`${coin.item.name} Sparkline`} />
+              </div>
             </div>
-            <h3 className="you-may-like-heading">Trending Coins</h3>
-            <div className="carousel-container">
-                <button className="nav-button prev-button">&#10094;</button>
-                <div className="carousel">
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                           
-                            <span>Ethereum</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$200</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                        
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="crypto-item">
-                         
-                            <span>Bitcoin</span>
-                        </div>
-                        <div className="price-info">
-                            <span>+$500</span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </div>
-                    </div>
-                </div>
-                <button className="nav-button next-button">&#10095;</button>
-            </div>
+          ))}
         </div>
-    );
-}
+        <button className="nav-button next-button" onClick={handleNextButtonClick}>
+          &#10095;
+        </button>
+      </div>
 
-export default Footer;
+      {/* Repeat the same section for another set of trending coins */}
+      <div className="footer-carousel">
+        <h3 className="you-may-like-heading">Trending Coins</h3>
+        <button className="nav-button prev-button" onClick={handlePrevButtonClick}>
+          &#10094;
+        </button>
+        <div className="carousel-container" style={{ transform: `translateX(-${scrollPosition}px)` }}>
+          {trendingCoins.map((coin) => (
+            <div className="carousel-item" key={coin.item.id}>
+              <div className="crypto-item">
+                <img src={coin.item.thumb} alt={`${coin.item.name} Logo`} />
+                <span>{coin.item.symbol}</span>
+              </div>
+              <div className="price-info">
+                <span>{coin.item.data.price}</span>
+                <img src={coin.item.sparkline} alt={`${coin.item.name} Sparkline`} />
+              </div>
+            </div>
+          ))}
+        </div>
+        <button className="nav-button next-button" onClick={handleNextButtonClick}>
+          &#10095;
+        </button>
+      </div>
+    </div>
+  );
+};
 
+export default FooterCarousel;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './navbar.css';
 import logoImage from './logo.png';
 
 function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the width as per your mobile breakpoint
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <div>
       <nav className="navbar">
         <div className="logo-container">
           <img src={logoImage} alt="Logo" className="logo-img" />
         </div>
-        <div className="nav-links-container">
+        {isMobile && (
+          <div className="menu-icon" onClick={toggleDrawer}>
+            <div className={`bar ${isDrawerOpen ? 'active' : ''}`}></div>
+            <div className={`bar ${isDrawerOpen ? 'active' : ''}`}></div>
+            <div className={`bar ${isDrawerOpen ? 'active' : ''}`}></div>
+          </div>
+        )}
+        {!isMobile && (
+          <div className="nav-links-container">
+            <a href="/" className="navbar-link">Crypto Taxes</a>
+            <a href="/" className="navbar-link">Free Tools</a>
+            <a href="/" className="navbar-link">Resource Center</a>
+            <a href="/" className="navbar-link-gs">Get Started</a>
+          </div>
+        )}
+      </nav>
+      {isMobile && (
+        <div className={`nav-drawer ${isDrawerOpen ? 'open' : ''}`}>
           <a href="/" className="navbar-link">Crypto Taxes</a>
           <a href="/" className="navbar-link">Free Tools</a>
           <a href="/" className="navbar-link">Resource Center</a>
           <a href="/" className="navbar-link-gs">Get Started</a>
         </div>
-      </nav>
+      )}
       <div className="additional-text">
         Cryptocurrencies &gt;&gt; Bitcoin
       </div>
@@ -24,4 +63,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
